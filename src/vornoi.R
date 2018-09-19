00472653e61ed9afc8fb1433635a764df89ec452
pandorina1013 <pandorina463@gmail.com>
2018-09-19 13:50:51 +0900

    tidyに一部修正

--- src/vornoi.R
+++ src/vornoi.R
@@ -3,25 +3,30 @@ load.packages <- c(
   "geojsonio",
   "deldir",
   "maptools")
-
 new.packages <- load.packages[!(load.packages %in% installed.packages()[,"Package"])]
 if(length(new.packages)) install.packages(new.packages, dependencies = T)
 lapply(load.packages, require, character.only = TRUE)
 
 points.df <- geojson_read("data/TBS.geojson", what = "sp") %>% 
   data.frame()
-w <- deldir(points.df$coords.x1, points.df$coords.x2, rw=c(min(points.df$coords.x1), max(points.df$coords.x1), min(points.df$coords.x2), max(points.df$coords.x2))) %>% 
+tl <- deldir(
+  points.df$coords.x1, 
+  points.df$coords.x2, 
+  rw=c(min(points.df$coords.x1), 
+       max(points.df$coords.x1), 
+       min(points.df$coords.x2), 
+       max(points.df$coords.x2))
+  ) %>% 
   tile.list()
-polys <- vector(mode='list', length = length(w))
-
+polys <- vector(mode='list', length = length(tl))
 for (i in seq(along=polys)) {
-  pcrds<-cbind(w[[i]]$x, w[[i]]$y)
-  pcrds<-rbind(pcrds, pcrds[1,])
-  polys[[i]]<-Polygons(list(Polygon(pcrds)), ID=as.character(i))
+  polys[[i]] <- cbind(tl[[i]]$x, tl[[i]]$y) %>% 
+    rbind(.[1,]) %>% 
+    Polygon() %>% 
+    list() %>% 
+    Polygons(ID=as.character(i))
 }
-
 voronoi <- polys %>% 
-  SpatialPolygons %>% 
-  SpatialPolygonsDataFrame(data = data.frame(x=points.df$coords.x1,y=points.df$coords.x2,storename=points.df$Name,
-                                                     row.names=sapply(slot(., 'polygons'),function(x) slot(x, 'ID'))))
+  SpatialPolygons() %>% 
+  SpatialPolygonsDataFrame(data.frame(x=points.df$coords.x1,y=points.df$coords.x2))
 writeSpatialShape(voronoi, fn = "data/vornoi-TBS")
