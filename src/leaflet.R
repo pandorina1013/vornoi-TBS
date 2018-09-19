280a6f958edc7e6ff9403810d41235e0a44afd47
pandorina1013 <pandorina463@gmail.com>
2018-09-19 17:18:25 +0900

    typo...

--- src/leaflet.R
+++ src/leaflet.R
@@ -7,10 +7,10 @@ new.packages <- load.packages[!(load.packages %in% installed.packages()[,"Packag
 if(length(new.packages)) install.packages(new.packages, dependencies = T)
 lapply(load.packages, require, character.only = TRUE)
 
-vornoi <- geojson_read("data/vornoi-TBS.geojson", what = "sp")
+voronoi <- geojson_read("data/voronoi-TBS.geojson", what = "sp")
 points <- geojson_read("data/TBS.geojson", what = "sp")
 
-vornoi.map <- leaflet(vornoi) %>%
+voronoi.map <- leaflet(voronoi) %>%
   addTiles() %>%
   setView(lng=139.7320, lat=35.6590, zoom = 12) %>%
   addPolygons(weight = 1)
